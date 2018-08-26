1b50765c05b45500a9dfd89f4791c892a3a2c829
Juan Pablo Peñaloza Botero <juan-penaloza@javeriana.edu.co>
2018-08-25 22:10:17 -0500

    Added dummy test and edited travis

--- App/tests.py
+++ App/tests.py
@@ -1,3 +1,7 @@
-#   from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+
+class DummyTest(TestCase):
+
+    def test_travis(self):
+        self.assertEqual('Hello', 'Hello')
