575a3a4c77699c26512af331c40e493de512b040
Johan Murillo <johan_smc@hotmail.com>
2018-09-02 14:22:01 -0500

    Add get all users

--- App/user/view.py
+++ App/user/view.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import User
 from rest_framework import status, viewsets
 from App.user.serializer import UserSerializer, UserModelSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
+from App.user.model import UserModel
 
 
 class UserViewSet(viewsets.ViewSet):
@@ -16,6 +20,19 @@ class UserViewSet(viewsets.ViewSet):
             )
         return Response(
             user_serializer.errors,
+            status=status.HTTP_200_OK
+        )
+
+    # Retrives all user from de db
+    def get_all_user(self, request):
+        all_user = User.objects.all()
+
+        users_serializer = UserSerializer(
+            all_user,
+            many=True
+        )
+        return Response(
+            users_serializer.data,
             status=status.HTTP_200_OK
         )
 
