5f702da8f1946386fccdcd2a16c2a0d9d4baea08
William Moreno <williammorenoprieto@gmail.com>
2018-09-20 22:10:13 -0500

    .

--- App/activity/model.py
+++ App/activity/model.py
@@ -10,4 +10,8 @@ class Activity(models.Model):
     due_date = models.DateTimeField()
     max_participants = models.IntegerField()
     visibility = models.BooleanField()
+<<<<<<< HEAD
     # User_Activity_id = models.IntegerField()
+=======
+    #User_Activity_id = models.IntegerField()
+>>>>>>> parent of 83bee6f... Activity v3.0 cascade
--- App/views.py
+++ App/views.py
@@ -6,5 +6,10 @@ from App.auth.view import ObtainExpiringAuthToken
 
 user = UserViewSet.as_view(dict(post='create', get='get_all_user'))
 auth = ObtainExpiringAuthToken.as_view()
+<<<<<<< HEAD
 activity_exact = ActivityView.as_view(dict(get='activity_exact', put='activity_exact', delete='activity_exact'))
 activity = ActivityView.as_view(dict(post='create', get='activity_list'))
+=======
+
+
+>>>>>>> parent of 83bee6f... Activity v3.0 cascade
