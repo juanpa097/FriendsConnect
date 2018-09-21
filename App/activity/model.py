6da9a6bddd816cfa424a85bbff5be0aa410189d7
William Moreno <williammorenoprieto@gmail.com>
2018-09-20 22:15:53 -0500

    .

--- App/activity/model.py
+++ App/activity/model.py
@@ -11,7 +11,11 @@ class Activity(models.Model):
     max_participants = models.IntegerField()
     visibility = models.BooleanField()
 <<<<<<< HEAD
+<<<<<<< HEAD
     # User_Activity_id = models.IntegerField()
+=======
+    #User_Activity_id = models.IntegerField()
+>>>>>>> parent of 83bee6f... Activity v3.0 cascade
 =======
     #User_Activity_id = models.IntegerField()
 >>>>>>> parent of 83bee6f... Activity v3.0 cascade
