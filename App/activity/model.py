e315920992fef075ecb2398a8966eb50aa8f2b4f
William Moreno <williammorenoprieto@gmail.com>
2018-09-20 22:17:45 -0500

    .

--- App/activity/model.py
+++ App/activity/model.py
@@ -12,7 +12,11 @@ class Activity(models.Model):
     visibility = models.BooleanField()
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     # User_Activity_id = models.IntegerField()
+=======
+    #User_Activity_id = models.IntegerField()
+>>>>>>> parent of 83bee6f... Activity v3.0 cascade
 =======
     #User_Activity_id = models.IntegerField()
 >>>>>>> parent of 83bee6f... Activity v3.0 cascade
--- App/views.py
+++ App/views.py
@@ -8,10 +8,14 @@ user = UserViewSet.as_view(dict(post='create', get='get_all_user'))
 auth = ObtainExpiringAuthToken.as_view()
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 activity_exact = ActivityView.as_view(dict(get='activity_exact', put='activity_exact', delete='activity_exact'))
 activity = ActivityView.as_view(dict(post='create', get='activity_list'))
 =======
 =======
+=======
+
+>>>>>>> parent of 83bee6f... Activity v3.0 cascade
 
 >>>>>>> parent of 83bee6f... Activity v3.0 cascade
 
