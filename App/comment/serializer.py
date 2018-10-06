06ecef4f7f46402915d6dbe1e9097e2617a7f7a8
Ivan Dario Sandoval <sandoval.i@javeriana.edu.co>
2018-10-05 23:40:14 -0500

    commit para trabajar en la u

--- App/comment/serializer.py
+++ App/comment/serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from App.comment.model import Comment
+from App.user.serializer import UserSerializer
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Comment
-        fields = ('description', 'date_created')
+        fields = ('description', 'date_created', 'user', 'replies')
